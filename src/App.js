6e8bf65b3a02c369ccbb1c618323838fbc888f93
KristaaP <krista41319@gmail.com>
2024-08-06 15:27:59 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import weatherk from "./weatherk";
+import Weatherk from "./Weatherk";
 import "./App.css";
 import React from "react";
 
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Hello from React!</h1>
-        <weatherk />
+        <Weatherk />
       </header>
     </div>
   );
